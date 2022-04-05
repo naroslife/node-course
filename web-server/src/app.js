98eee4ac15b38847693b1226d1346bb240926812
nagy4 <robert.nagy@knapp.com>
2022-04-05 18:22:50 +0200

    Serve up webpage.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,29 @@
+const path = require('path');
 const express = require('express');
 
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('Hello Express!')
-})
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath))
 
-app.get()
+// app.get('', (req, res) => {
+//     res.send('Hello Express!')
+// })
+
+// app.get('/help', (req, res) => {
+//     res.send('Help page')
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About</h1><br>About page')
+// })
+
+app.get('/weather', (req, res) => {
+    res.send({
+        location: "Budapest",
+        forecast: "Clear"
+    })
+})
 
 const port = 3000
 app.listen(port, () => {
