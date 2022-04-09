780418cc8d169de4a83c4008bf3f92de21bc392e
Robert Nagy <robi54321@gmail.com>
2022-04-09 19:59:23 +0200

    Finish weather app.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,41 @@
 console.log('Client side javascript file loaded');
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     })
+// })
+
+fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log("Error: " + data.error);
+        }
+        console.log("Forecast: " + data.forecast);
+    })
+})
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+const locationParagraph = document.getElementById('locationParagraph')
+const forecastParagraph = document.querySelector('#forecastParagraph') // same as above
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = searchElement.value
+    forecastParagraph.textContent = ''
+    locationParagraph.textContent = ''
+    locationParagraph.className = 'loader'
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            locationParagraph.className = ''
+            locationParagraph.textContent = data.error
+            forecastParagraph.textContent = ''
+        } else {
+            locationParagraph.className = ''
+            locationParagraph.textContent = data.location
+            forecastParagraph.textContent = data.forecast
+        }
+    })
+})
+})
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const { forecast } = require('./utils/forecast');
+const { geocode } = require('./utils/geocode');
+
 
 const app = express()
 
@@ -31,38 +34,61 @@ app.use(express.static(publicPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Robert'
+        name: 'naroslife'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About',
-        name: 'Robert'
+        name: 'naroslife'
     })
 })
 
 app.get('/weather', (req, res) => {
-    console.log(req.query);
-    res.send({
-        location: "Budapest",
-        forecast: "Clear"
-    })
+    if (!req.query.address) {
+        return res.send({
+            error: "You must provide an address!"
+        })
+    }
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({error})
+        } else {
+            forecast(latitude, longitude, location, (error, forecast) => {
+                if (error) {
+                    return res.send({error})
+                }
+                res.send({
+                    forecast,
+                    location,
+                    address: req.query.address
+                })
+            })
+    
+        }
+    });
 })
 
 app.get('products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: "You must provide a search term!"
+        })
+    }
+    console.log(req.query.search);
     res.send({
         products: [] 
     })
 })
 
 app.get('/help/*', (req, res) => {
-    res.render('404', {errorMessage: "Help article not found!", name: "Robert", title:"404"})
+    res.render('404', {errorMessage: "Help article not found!", name: "naroslife", title:"404"})
 })
 
 // * get has to come last
 app.get('*', (req, res) => {
-    res.render('404', {errorMessage: "Page not found!", name: "Robert", title:"404"})
+    res.render('404', {errorMessage: "Page not found!", name: "naroslife", title:"404"})
 })
 
 const port = 3000
